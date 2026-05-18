35d6ff521ce2d4dff3a7ae018e2e3dd81cf06ac9
zhyi-dp <zhyi@dpai.com>
2026-05-18 11:54:38 +0800

    fix(wasm): embed standard library and recursively parse imports

--- src/wasm_api.cpp
+++ src/wasm_api.cpp
@@ -20,6 +20,83 @@ public:
     }
 };
 
+
+#include <fstream>
+#include <vector>
+#include <set>
+#include <memory>
+
+void parseSource(const std::string &filename,
+                 std::vector<std::unique_ptr<toka::Module>> &astModules,
+                 std::set<std::string> &visited,
+                 std::vector<std::string> &recursionStack,
+                 toka::SourceManager &sm,
+                 const std::vector<std::string> &searchPaths,
+                 const std::string &code_cstr = "") {
+  if (visited.count(filename)) return;
+  visited.insert(filename);
+  recursionStack.push_back(filename);
+
+  std::string resolvedPath = filename;
+  bool found = false;
+
+  if (!code_cstr.empty()) {
+      found = true;
+  } else if (std::ifstream(filename).good()) {
+      found = true;
+  } else if (filename.find(".tk") == std::string::npos && std::ifstream(filename + ".tk").good()) {
+      resolvedPath = filename + ".tk";
+      found = true;
+  } else {
+      std::vector<std::string> pathsToTry = {"lib/", "../lib/"};
+      for (const auto &p : searchPaths) {
+          pathsToTry.push_back(p.back() == '/' ? p : p + "/");
+      }
+      for (const auto &p : pathsToTry) {
+          std::string libPath = p + filename;
+          if (std::ifstream(libPath).good()) {
+              resolvedPath = libPath; found = true; break;
+          }
+          if (filename.find(".tk") == std::string::npos) {
+              libPath += ".tk";
+              if (std::ifstream(libPath).good()) {
+                  resolvedPath = libPath; found = true; break;
+              }
+          }
+      }
+  }
+
+  if (!found) {
+      toka::DiagnosticEngine::report(toka::DiagLoc{}, toka::DiagID::ERR_FILE_IO, "Could not open file: " + filename);
+      return;
+  }
+
+  toka::SourceLocation startLoc = code_cstr.empty() ? sm.loadFile(resolvedPath) : sm.addFile(resolvedPath, code_cstr);
+  if (startLoc.isInvalid()) return;
+  std::string code(sm.getBufferData(startLoc));
+
+  toka::Lexer lexer(code.c_str(), startLoc);
+  auto tokens = lexer.tokenize();
+
+  toka::Parser parser(tokens, resolvedPath);
+  auto module = parser.parseModule();
+
+  if (!module) return;
+
+  for (const auto &imp : module->Imports) {
+      std::string importPath = imp->PhysicalPath;
+      if (importPath.rfind("./", 0) == 0 || importPath.rfind("../", 0) == 0) {
+          size_t lastSlash = resolvedPath.find_last_of('/');
+          std::string parentDir = (lastSlash == std::string::npos) ? "." : resolvedPath.substr(0, lastSlash);
+          importPath = parentDir + "/" + importPath;
+      }
+      parseSource(importPath, astModules, visited, recursionStack, sm, searchPaths);
+  }
+
+  astModules.push_back(std::move(module));
+  recursionStack.pop_back();
+}
+
 extern "C" {
 
 EMSCRIPTEN_KEEPALIVE
@@ -40,20 +117,18 @@ const char* check_toka_code(const char* code_cstr) {
     toka::SourceManager sm;
     toka::DiagnosticEngine::init(sm);
 
-    // Create a virtual file in SourceManager
-    toka::SourceLocation startLoc = sm.addFile("playground.tk", code_cstr);
+    std::vector<std::unique_ptr<toka::Module>> astModules;
+    std::set<std::string> visited;
+    std::vector<std::string> recursionStack;
+    std::vector<std::string> searchPaths; // Emscripten will have access to virtual /lib
 
-    if (!startLoc.isInvalid()) {
-        toka::Lexer lexer(code_cstr, startLoc);
-        auto tokens = lexer.tokenize();
+    parseSource("playground.tk", astModules, visited, recursionStack, sm, searchPaths, code_cstr);
 
-        toka::Parser parser(tokens, "playground.tk");
-        auto module = parser.parseModule();
-
-        if (!toka::DiagnosticEngine::hasErrors() && module) {
-            toka::Sema sema;
-            sema.setBorrowCheckEnabled(true);
-            sema.checkModule(*module);
+    if (!toka::DiagnosticEngine::hasErrors()) {
+        toka::Sema sema;
+        sema.setBorrowCheckEnabled(true);
+        for (auto &mod : astModules) {
+            sema.checkModule(*mod);
         }
     }
 
