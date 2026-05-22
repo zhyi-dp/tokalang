61edb83df9ea61381e392cf8250dc46f7d8d6b8b
dp-aixball <aixball@qq.com>
2026-05-22 23:18:19 +0800

    [pass 256/256] Fix Windows shell32 linkage and executable suffix in build engine

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,7 @@ bool linkWithLLD(std::string objFile, std::vector<std::string> extraObjs, std::s
     for (const auto &extra : extraObjs) {
         cmd += " \"" + extra + "\"";
     }
-    cmd += " -o \"" + outputFile + "\" -lws2_32";
+    cmd += " -o \"" + outputFile + "\" -lws2_32 -lshell32";
     return system(cmd.c_str()) == 0;
 #elif defined(__APPLE__)
     args.push_back("-w");
